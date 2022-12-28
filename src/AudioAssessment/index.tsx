709ca0b4ba68191799c599d12573d0d920ce3092
daingo <daingo7211@gmail.com>
2022-12-28 15:50:36 +0700

    12

--- src/AudioAssessment/index.tsx
+++ src/AudioAssessment/index.tsx
@@ -1,20 +1,18 @@
-import { useEffect, useState } from "react";
-import { ResourceLayoutEnum } from "../enums/layout";
-import { AudioAssessmentContext } from "./ContextAudioAssessment";
+import {useEffect, useState} from "react";
+import {ResourceLayoutEnum} from "../enums/layout";
+import {AudioAssessmentContext} from "./ContextAudioAssessment";
 import DoAssessment from "./do";
-import { ResponseDefault } from "./view/type";
+import {ResponseDefault} from "./view/type";
 import ViewResource from "./view";
-import { useObserverHeight } from "./hooks/useObserverHeight";
-import { ACTION_POST_MESSAGE } from "../enums/action";
+import {useObserverHeight} from "./hooks/useObserverHeight";
+import {ACTION_POST_MESSAGE} from "../enums/action";
 
 function AudioAssessment() {
     const [data, setData] = useState<ResponseDefault | null>(
         new ResponseDefault()
     );
 
-    const [studentAssignmentId, setStudentAssignmentId] = useState<
-        number | undefined
-    >(undefined);
+    const [studentAssignmentId, setStudentAssignmentId] = useState<number | undefined>(undefined);
 
     const [layout, setLayout] = useState<ResourceLayoutEnum>(
         ResourceLayoutEnum.DO_ASSIGNMENT
@@ -29,34 +27,34 @@ function AudioAssessment() {
                 "FPR::: offsetHeight",
                 document.documentElement.offsetHeight
             );
-            switch (event.data.action) {
-                case ACTION_POST_MESSAGE.RESP_DATA:
-                    if (event.data.body.response) {
-                        setData(event.data.body.response);
-                    }
-
-                    if (event.data.body.layout) {
-                        setLayout(event.data.body.layout);
-                    }
-
-                    if (event.data.body.accessToken) {
-                        localStorage.setItem(
-                            "accessToken",
-                            event.data.body.accessToken
-                        );
-                    }
-
-                    if (event.data.body.studentAssignmentId) {
-                        setStudentAssignmentId(
-                            event.data.body.studentAssignmentId
-                        );
-                    }
-                    break;
-                case ACTION_POST_MESSAGE.FPR_SEND_AUDIO:
-                    break;
-                default:
-                    break;
+            if (event.data.body.response) {
+                setData(event.data.body.response);
             }
+
+            if (event.data.body.layout) {
+                setLayout(event.data.body.layout);
+            }
+
+            if (event.data.body.accessToken) {
+                localStorage.setItem(
+                    "accessToken",
+                    event.data.body.accessToken
+                );
+            }
+
+            if (event.data.body.studentAssignmentId) {
+                setStudentAssignmentId(
+                    event.data.body.studentAssignmentId
+                );
+            }
+            // switch (event.data.action) {
+            //     case ACTION_POST_MESSAGE.RESP_DATA:
+            //         break;
+            //     case ACTION_POST_MESSAGE.FPR_SEND_AUDIO:
+            //         break;
+            //     default:
+            //         break;
+            // }
         };
         window.addEventListener("message", fn);
         return () => {
@@ -83,9 +81,9 @@ function AudioAssessment() {
     const Component = (() => {
         switch (layout) {
             case ResourceLayoutEnum.VIEW_RESOURCE:
-                return <ViewResource />;
+                return <ViewResource/>;
             case ResourceLayoutEnum.DO_ASSIGNMENT:
-                return <DoAssessment />;
+                return <DoAssessment/>;
 
             default:
                 break;
