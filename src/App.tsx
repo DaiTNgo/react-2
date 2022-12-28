02b5bd5d5b6e243217b6da04a14cbe1faf068e6b
daingo <daingo7211@gmail.com>
2022-12-28 14:59:01 +0700

    migrate styled to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import AudioAssessment from "./AudioAssessment";
 import Modal from "./components/modal";
-import { ModalProvider } from "./context/ModalContext";
+import {ModalProvider} from "./context/ModalContext";
 
 function App() {
     return (
         <>
-            <AudioAssessment />
-            <Modal />
+            <AudioAssessment/>
+            <Modal/>
         </>
     );
 }
 
+
 function ProviderApp() {
     return (
         <ModalProvider>
-            <App />
+            <App/>
         </ModalProvider>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,44 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+// const template = document.createElement("template");
+// template.innerHTML = `
+//   <style>
+//   </style>
+//   <script></script>
+// `;
+//
+// export default class BuyTools extends HTMLElement {
+//     constructor() {
+//         super();
+//         // @ts-ignore
+//     }
+//     connectedCallback() {
+//         //@ts-ignore
+//         // this.root =
+//         this.attachShadow({ mode: "open" });
+//         // @ts-ignore
+//         this.shadowRoot.appendChild(template.content.cloneNode(true));
+//         //@ts-ignore
+//         this.render();
+//
+//         // @ts-ignore
+//         // this.root.append(template.content.cloneNode(true));
+//     }
+//
+//     render() {
+//         // @ts-ignore
+//         ReactDOM.createRoot(this.shadowRoot as HTMLElement).render(
+//             <React.StrictMode>
+//                 <App />
+//             </React.StrictMode>
+//         );
+//     }
+// }
+//
+// customElements.define("my-buy-tools", BuyTools);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
