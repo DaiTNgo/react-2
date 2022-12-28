e8efcf9ae76a44c37866adf1440c39d0b5d522e1
daingo <daingo7211@gmail.com>
2022-12-28 15:29:45 +0700

    revise

--- src/main.tsx
+++ src/main.tsx
@@ -3,43 +3,43 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const template = document.createElement("template");
-template.innerHTML = `
-  <style>
-  </style>
-  <div id="fpr-template">
-  </div>
-  <script></script>
-`;
-
-class FPRAudioAssessment extends HTMLElement {
-    constructor() {
-        super();
-    }
-
-    connectedCallback() {
-        this.attachShadow({mode: "open"});
-        // @ts-ignore
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-        //@ts-ignore
-        this.root = this.shadowRoot.querySelector('#fpr-template')
-        this.render();
-    }
-
-    render() {
-        // @ts-ignore
-        ReactDOM.createRoot(this.root as HTMLElement).render(
-            <React.StrictMode>
-                <App/>
-            </React.StrictMode>
-        );
-    }
-}
-
-customElements.define("fpr-audio-assessment", FPRAudioAssessment);
+// const template = document.createElement("template");
+// template.innerHTML = `
+//   <style>
+//   </style>
+//   <div id="fpr-template">
+//   </div>
+//   <script></script>
+// `;
 //
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-// );
+// class FPRAudioAssessment extends HTMLElement {
+//     constructor() {
+//         super();
+//     }
+//
+//     connectedCallback() {
+//         this.attachShadow({mode: "open"});
+//         // @ts-ignore
+//         this.shadowRoot.appendChild(template.content.cloneNode(true));
+//         //@ts-ignore
+//         this.root = this.shadowRoot.querySelector('#fpr-template')
+//         this.render();
+//     }
+//
+//     render() {
+//         // @ts-ignore
+//         ReactDOM.createRoot(this.root as HTMLElement).render(
+//             <React.StrictMode>
+//                 <App/>
+//             </React.StrictMode>
+//         );
+//     }
+// }
+//
+// customElements.define("fpr-audio-assessment", FPRAudioAssessment);
+//
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
