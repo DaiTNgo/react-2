732a2bc3de6b44adfb24a9099cb199a1e04af1b8
daingo <daingo7211@gmail.com>
2023-01-12 09:02:22 +0700

    grade + regrade

--- src/AudioAssessment/re-grade/utils.ts
+++ src/AudioAssessment/re-grade/utils.ts
@@ -1,10 +1,8 @@
 import { ResponseDefault } from "../view/type";
 
-export const getResultData = (
-    data: ResponseDefault & { gradingResults: any[] }
-) => {
+export const getResultData = (data: ResponseDefault) => {
     try {
-        return data.gradingResults;
+        return data.studentAssignment.gradingResults;
     } catch (err) {
         return [];
     }
--- src/AudioAssessment/view/type.ts
+++ src/AudioAssessment/view/type.ts
@@ -158,6 +158,94 @@ export class ResponseDefault extends Response {
         assignableResource: false,
         audioAssessment: true,
     };
+
+    studentAssignment = {
+        studentId: 730248,
+        firstName: "stu1",
+        lastName: "stu1",
+        studentAssignmentId: 14757041,
+        audioRecordedUrl:
+            "https://assets.sadlierconnect.com/cqa/AudioAssessment/730248/14757041_audio_recorded.wav?Expires=1673498300&Signature=bXEGAgC~-nW1823qlwq6dZ4sDULwZa3XlxKQFf7NxlNLSi8eKh58n0i02zf~o~hxb8B4nWtjGmYBApNXx705R-a-I4P2ZJHG13yQiLVmFdzC3rfN0WOdjdiO67cuXqAGMurlXLGKU3bz2fXUlyDLPAlHldEY8BJXHDbN5OsLYNfGPSD6CBeGIqoPPRihOR~OHtpPoAaCnQ9LlCbnom0v8-ogXHuOAlP55uZfx4Do2zUCIs~C2N8daR~IsuXi2sZTYFyl-Bf9pbqE9KzQTEy72PTIVqbMpvopk4HE-310wi6FNXn-Lgvg3BAaJtaxqE5OdJ~IUDsiWnQPrrQXzrO8Lg__&Key-Pair-Id=APKAJLNAMKK3PO57GWDA",
+        comments: "",
+        speedScore: -1,
+        accuracyScore: 0,
+        fluencyScore: 0,
+        totalScore: -1,
+        gradingResults: [
+            {
+                word: "diot",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "spoud",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "clar",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "foy",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "jern",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "moof",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "lurst",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "porth",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "N/A",
+            },
+            {
+                word: "stook",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "NULL",
+            },
+            {
+                word: "flirch",
+                correct: "incorrect",
+                accuracy: "idle",
+                fluency: "idle",
+                comment: "undefined",
+            },
+        ],
+        phonicsAssessmentResponseId: 69,
+    };
+
     questionBean = {
         listQuestion: [
             {
