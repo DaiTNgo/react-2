3a3cdf3bbb66a5c05571feb2674f9153747f3d49
daingo <daingo7211@gmail.com>
2023-02-06 08:28:39 +0700

    fix

--- src/AudioAssessment/index.tsx
+++ src/AudioAssessment/index.tsx
@@ -19,14 +19,12 @@ const FallBack = () => {
 
 function AudioAssessment() {
     const [scale, setScale] = useState(1);
-    const [data, setData] = useState<ResponseDefault | null>(
-        new ResponseDefault()
-    );
+    const [data, setData] = useState<ResponseDefault | null>(null);
 
     const [urlRecordStudent, setUrlRecordStudent] = useState("");
 
     const [layout, setLayout] = useState<ResourceLayoutEnum>(
-        ResourceLayoutEnum.GRADING
+        ResourceLayoutEnum.VIEW_RESOURCE
     );
 
     const handlePrint = () => {
