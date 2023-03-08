d22006aeaa3a48b44a6c476e14913a679ee1db50
DaiNgo <daingo7211@gmail.com>
2023-03-08 18:38:00 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // mode: "production",
-    // esbuild: {
-    //     drop: ["console", "debugger"],
-    // },
+    mode: "production",
+    esbuild: {
+        drop: ["console", "debugger"],
+    },
 
     /**TODO: Production*/
     // base: "/sc-content/javascript/fpr_1.7",
-    base: "./",
+    // base: "./",
 });
